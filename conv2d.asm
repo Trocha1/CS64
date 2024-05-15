75a40e0ce4c4851f90a01ff2067d70d0f019f80a
Tristan <t_rocha@ucsb.edu>
2024-05-14 22:15:25 -0700

    lab06 done

--- conv2d.asm
+++ conv2d.asm
@@ -83,6 +83,16 @@ conv:
 
 
 conv2d:
+    sub $sp $sp 36
+    sw $s0 0($sp)
+    sw $s1 4($sp)
+    sw $s2 8($sp)
+    sw $s3 12($sp)
+    sw $s4 16($sp)
+    sw $s5 20($sp)
+    sw $s6 24($sp)
+    sw $s7 28($sp)
+
     move $s7 $a0
     move $s1 $a1
     move $s2 $a2
@@ -184,6 +194,15 @@ conv2d:
 
     return_2d:
         add $sp $sp $s4
+        lw $s0 0($sp)
+        lw $s1 4($sp)
+        lw $s2 8($sp)
+        lw $s3 12($sp)
+        lw $s4 16($sp)
+        lw $s5 20($sp)
+        lw $s6 24($sp)
+        lw $s7 28($sp)
+        add $sp $sp 36
 
         jr $ra
 
